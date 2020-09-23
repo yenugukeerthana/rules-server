eb609e383c03f2ed6f37fe0df65ca6c3a4e6757a
Vinod Deolal <vinoddeolal@gmail.com>
2020-09-23 18:03:10 +0530

    #6 | adding circleci job for deployment

--- server/db.js
+++ server/db.js
@@ -5,11 +5,11 @@ var options = {
 
 var pgp = require('pg-promise')(options);
 const connectionString = {
-  host: 'localhost', // 'localhost' is the default;
-  port: 5432, // 5432 is the default;
-  database: 'openchs',
-  user: 'amol',
-  password: ''
+  host: 'localhost',
+  port: 5432,
+  database: process.env.OPENCHS_DATABASE || 'openchs',
+  user: process.env.OPENCHS_DATABASE_USER || 'openchs',
+  password: process.env.OPENCHS_DATABASE_PASSWORD || 'password',
 };
 var db = pgp(connectionString);
 export default db;
