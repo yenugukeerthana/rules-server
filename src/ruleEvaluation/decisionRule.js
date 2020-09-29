654bd561742b8f201e98fefca7b1998e19ba6863
Vinay Venu <ijkvinay@gmail.com>
2020-09-29 16:05:36 +0530

    Use safeEval for secure eval of rules

--- src/ruleEvaluation/decisionRule.js
+++ src/ruleEvaluation/decisionRule.js
@@ -1,6 +1,7 @@
 import * as rulesConfig from 'rules-config';
 import lodash from 'lodash';
 import moment from 'moment';
+import safeEval from 'safe-eval';
 
 export const decisionRule = async (rule,entity) => {
     const defaultDecisions = {
@@ -8,7 +9,7 @@ export const decisionRule = async (rule,entity) => {
         "encounterDecisions": [],
         "registrationDecisions": []
     };
-    const ruleFunc = eval(rule);
+    const ruleFunc = safeEval(rule);
     const ruleDecisions = ruleFunc({
         params: {decisions: defaultDecisions, entity},
         imports: {rulesConfig, lodash, moment}
@@ -17,7 +18,7 @@ export const decisionRule = async (rule,entity) => {
 }
 
 export const visitScheduleRule = async (rule,entity,scheduledVisits) => {
-    const ruleFunc = eval(rule);
+    const ruleFunc = safeEval(rule);
     const nextVisits = ruleFunc({
         params: { visitSchedule: scheduledVisits, entity },
         imports: { rulesConfig, lodash, moment }
@@ -27,7 +28,7 @@ export const visitScheduleRule = async (rule,entity,scheduledVisits) => {
 
 export const checkListRule = async (rule,entity) => {
     const allChecklistDetails = JSON.parse('[{"uuid":"123-3454-56756-789","name":"Vaccination","items":[{"uuid":"123-456-789-5456"}]}]');
-    const ruleFunc = eval(rule);
+    const ruleFunc = safeEval(rule);
     const nextVisits = ruleFunc({
         params: { checklistDetails: allChecklistDetails, entity },
         imports: { rulesConfig, lodash, moment }
