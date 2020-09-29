589619108d341a57c57504d593a84f2a8d1b50da
Hiren Thacker <hithacker@gmail.com>
2020-09-29 14:45:51 +0530

    #0 | removing dead code

--- src/service/BaseService.js
+++ src/service/BaseService.js
@@ -4,33 +4,6 @@ class BaseService {
     constructor(){
         this.db = db;
     }
-
-    async findAllByKey(keyName, value, schemaName) {
-        return await this.findAllByCriteria(`${keyName}='${value}'`, schemaName);
-    }
-
-    async findAllByCriteria(filterCriteria, schema) {
-        return await this.db.any('SELECT * FROM '+ schema+ ' WHERE '+filterCriteria);
-    }
-
-    async findAll(schema = this.getSchema()) {
-        return await this.db.any(`SELECT * FROM `+schema);
-    }
-
-    async findByKey(keyName, value, schemaName = this.getSchema()) {
-        const entities = await this.findAllByKey(keyName, value, schemaName);
-        return this.getReturnValue(entities);
-    }
-
-    getReturnValue(entities) {
-        if (entities.length === 0) return undefined;
-        if (entities.length === 1) return entities[0];
-        return entities;
-    }
-
-    getSchema() {
-        throw "SchemaName should be passed";
-    }
 }
 
 export default BaseService;
