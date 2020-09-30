5eb256d90fd9531781bb435a141f4064c9568ff2
Hiren Thacker <hithacker@gmail.com>
2020-09-30 14:21:37 +0530

    avni-server#119 | removed db look up for visit schedule rules

--- src/controllers/programEnrolmentController.js
+++ src/controllers/programEnrolmentController.js
@@ -16,19 +16,17 @@ const decisionRules = (req, res, next) => {
       });
 }
 
-const visitScheduleRules = (req, res, next) => {
-  rulesService.findRulesById(req.body, res, next) 
-    .then(async function (data) {
-        const rulevalidated = await programEnrolment(JSON.parse(JSON.stringify(data))[0].rules,req.body);
-        res.status(200)
-            .json({
-                status: 'success',
-                visitSchedules: rulevalidated
-            });
-    })
-      .catch(function (err) {
-        return next(err);
+const visitScheduleRules = async (req, res, next) => {
+  try {
+    const rulevalidated = await programEnrolment(req.body.rule.code, req.body);
+    res.status(200)
+      .json({
+        status: 'success',
+        visitSchedules: rulevalidated
       });
+  } catch (err) {
+    return next(err);
+  }
 }
 
 const checkListRules = (req, res, next) => {
