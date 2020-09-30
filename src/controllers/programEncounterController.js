b6b7ad5b9664e232a96ff26376a76be02a6d8c49
Hiren Thacker <hithacker@gmail.com>
2020-09-30 08:48:29 +0530

    avni-server#119 | Pass the rule directly from java server

--- src/controllers/programEncounterController.js
+++ src/controllers/programEncounterController.js
@@ -16,19 +16,17 @@ const generateRules = (req, res, next) => {
       });
 }
 
-const visitScheduleRules = (req, res, next) => {
-  rulesService.findRulesById(req.body, res, next)
-    .then(async function (data) {
-        const rulevalidated = await programEncounter(JSON.parse(JSON.stringify(data))[0].rules,req.body);
-        res.status(200)
-            .json({
-                status: 'success',
-                visitSchedules: rulevalidated
-            });
-    })
-      .catch(function (err) {
-        return next(err);
+const visitScheduleRules = async (req, res, next) => {
+  try {
+    const rulevalidated = await programEncounter(req.body.rule.code, req.body);
+    res.status(200)
+      .json({
+        status: 'success',
+        visitSchedules: rulevalidated
       });
+  } catch (err) {
+    return next(err);
+  }
 }
 
 module.exports = {
