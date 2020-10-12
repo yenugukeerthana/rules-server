5777460ee52fac87680d881f789631abf7bd965b
Hiren Thacker <hithacker@gmail.com>
2020-10-12 16:43:44 +0530

    Fixing failing tests

--- src/ruleEvaluation/decisionRule.test.js
+++ src/ruleEvaluation/decisionRule.test.js
@@ -1,24 +1,24 @@
 import {checkListRule, decisionRule, visitScheduleRule} from "./decisionRule";
 
 describe('decisionRule', () => {
-  it('should not be able to access node.js apis', async () => {
-    await expect(decisionRule('() => { var fs = require("fs"); fs.readdirSync("/", "utf8")}', 'dummy'))
-      .rejects.toThrow()
+  it('should not be able to access node.js apis', () => {
+     expect(() => decisionRule('() => { var fs = require("fs"); fs.readdirSync("/", "utf8")}', 'dummy'))
+      .toThrow()
   });
 });
 
 describe('visitScheduleRule', () => {
-  it('should not be able to access node.js apis', async () => {
-    await expect(visitScheduleRule('() => { var fs = require("fs"); fs.readdirSync("/", "utf8")}', 'dummy'))
-      .rejects.toThrow()
+  it('should not be able to access node.js apis', () => {
+    expect(() => visitScheduleRule('() => { var fs = require("fs"); fs.readdirSync("/", "utf8")}', 'dummy'))
+        .toThrow()
   });
 });
 
 
 describe('checkListRule', () => {
-  it('should not be able to access node.js apis', async () => {
-    await expect(checkListRule('() => { var fs = require("fs"); fs.readdirSync("/", "utf8")}', 'dummy'))
-      .rejects.toThrow()
+  it('should not be able to access node.js apis', () => {
+    expect(() => checkListRule('() => { var fs = require("fs"); fs.readdirSync("/", "utf8")}', 'dummy'))
+        .toThrow()
   });
 });
 
