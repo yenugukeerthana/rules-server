3689b4b7d0d7ebbe1c1c09afaf865afff6df48c7
Vinod Deolal <vinoddeolal@gmail.com>
2022-01-03 16:44:24 +0530

    #691[web-app] | map the group information with the subject

--- src/models/individualModel.js
+++ src/models/individualModel.js
@@ -1,53 +1,97 @@
-import {AddressLevel, Gender, Individual, ModelGeneral as General, SubjectType} from "openchs-models";
+import {AddressLevel, Gender, GroupSubject, Individual, ModelGeneral as General, SubjectType, GroupRole} from "openchs-models";
 import {mapObservations} from "./observationModel";
 import {mapEncounter} from "./encounterModel";
 import {mapEntityApprovalStatus} from "./entityApprovalStatusModel";
-import {isEmpty, isNil, map} from 'lodash';
+import {isNil, map} from 'lodash';
 import {mapProgramEnrolment} from "./programEnrolmentModel";
 
+function mapBasicSubject(individualDetails) {
+    const individual = General.assignFields(
+        individualDetails,
+        new Individual(),
+        ["uuid", "firstName", "lastName"],
+        ["registrationDate", "dateOfBirth"]
+    );
+
+    const subjectType = mapSubjectType(individualDetails.subjectType);
+    individual.subjectType = subjectType;
+
+    if (subjectType.isPerson()) {
+        const gender = new Gender();
+        gender.name = individualDetails.gender.name;
+        gender.uuid = individualDetails.gender.uuid;
+        individual.gender = gender;
+    }
+
+    const addressLevel = new AddressLevel();
+    addressLevel.uuid = individualDetails.lowestAddressLevel.uuid;
+    addressLevel.name = individualDetails.lowestAddressLevel.name;
+    individual.lowestAddressLevel = addressLevel;
+
+    individual.observations = mapObservations(individualDetails["observations"]);
+    return individual;
+}
+
 export const mapIndividual = individualDetails => {
-  const individual = General.assignFields(
-    individualDetails,
-    new Individual(),
-    ["uuid", "firstName", "lastName"],
-    ["registrationDate","dateOfBirth"]
-  );
+    const individual = mapBasicSubject(individualDetails);
+    individual.latestEntityApprovalStatus = mapEntityApprovalStatus(individualDetails.latestEntityApprovalStatus);
+    if (!isNil(individualDetails.encounters)) {
+        individual.encounters = map(individualDetails.encounters, encounter => mapEncounter(encounter))
+    } else {
+        individual.encounters = [];
+    }
 
-  const subjectType = mapSubjectType(individualDetails.subjectType);
-  individual.subjectType = subjectType;
+    if (!isNil(individualDetails.enrolments)) {
+        individual.enrolments = map(individualDetails.enrolments, enrolment => mapProgramEnrolment(enrolment))
+    } else {
+        individual.enrolments = [];
+    }
 
-  if(subjectType.isPerson()) {
-    const gender = new Gender();
-    gender.name = individualDetails.gender.name;
-    gender.uuid = individualDetails.gender.uuid;
-    individual.gender = gender;
-  }
+    if (!isNil(individualDetails.groups)) {
+        individual.groups = map(individualDetails.groups, groupSubject => mapGroupSubject(groupSubject))
+    } else {
+        individual.groups = [];
+    }
 
-  const addressLevel = new AddressLevel();
-  addressLevel.uuid = individualDetails.lowestAddressLevel.uuid;
-  addressLevel.name = individualDetails.lowestAddressLevel.name;
-  individual.lowestAddressLevel = addressLevel;
-
-  individual.observations = mapObservations(individualDetails["observations"]);
-  individual.latestEntityApprovalStatus = mapEntityApprovalStatus(individualDetails.latestEntityApprovalStatus);
-  if (!isNil(individualDetails.encounters)) {
-    individual.encounters = map(individualDetails.encounters, encounter => mapEncounter(encounter))
-  } else {
-    individual.encounters = [];
-  }
-
-  if (!isNil(individualDetails.enrolments)) {
-    individual.enrolments = map(individualDetails.enrolments, enrolment => mapProgramEnrolment(enrolment))
-  } else {
-    individual.enrolments = [];
-  }
-
-  return individual;
+    return individual;
 };
 
 const mapSubjectType = request => {
-  if (isNil(request)) {
-    return new SubjectType();
-  }
-  return General.assignFields(request, new SubjectType(), ["uuid", "name", "type"]);
+    if (isNil(request)) {
+        return new SubjectType();
+    }
+    return General.assignFields(request, new SubjectType(), ["uuid", "name", "type"]);
+};
+
+
+const mapGroupSubject = resource => {
+    const groupSubjectEntity = General.assignFields(
+        resource,
+        new GroupSubject(),
+        ["uuid", "voided"],
+        ["membershipStartDate", "membershipEndDate"]
+    );
+    const groupSubject = mapIndividual(resource.groupSubject);
+    const memberSubject = mapIndividual(resource.memberSubject);
+    const groupRole = mapGroupRole(resource.groupRole);
+    groupSubjectEntity.groupSubject = groupSubject;
+    groupSubjectEntity.memberSubject = memberSubject;
+    groupSubjectEntity.groupRole = groupRole;
+    return groupSubjectEntity;
+};
+
+const mapGroupRole = resource => {
+    const groupSubjectType = mapSubjectType(resource.groupSubjectType);
+    const memberSubjectType = mapSubjectType(resource.memberSubjectType);
+    const groupRole = General.assignFields(resource, new GroupRole(), [
+        "uuid",
+        "role",
+        "primary",
+        "maximumNumberOfMembers",
+        "minimumNumberOfMembers",
+        "voided",
+    ]);
+    groupRole.groupSubjectType = groupSubjectType;
+    groupRole.memberSubjectType = memberSubjectType;
+    return groupRole;
 };
