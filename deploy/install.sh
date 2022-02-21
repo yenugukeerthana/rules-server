875ce4633df595a642251c44bbb0922313fd36fc
Vinod Deolal <vinoddeolal@gmail.com>
2022-02-21 17:26:48 +0530

    update the env variable forcefully

--- deploy/install.sh
+++ deploy/install.sh
@@ -19,4 +19,4 @@ chown -R rules:rules /opt/rules-server
 chgrp -R rules /opt/rules-server
 chmod -R 770 /opt/rules-server
 cd /opt/rules-server
-sudo -H -u rules bash -c "PM2_HOME=/etc/pm2deamon pm2 stop rules-server && PM2_HOME=/etc/pm2deamon OPENCHS_UPLOAD_USER_USER_NAME=$OPENCHS_UPLOAD_USER_USER_NAME OPENCHS_UPLOAD_USER_PASSWORD=$OPENCHS_UPLOAD_USER_PASSWORD pm2 start app.js --name rules-server"
+sudo -H -u rules bash -c "PM2_HOME=/etc/pm2deamon pm2 stop rules-server && PM2_HOME=/etc/pm2deamon OPENCHS_UPLOAD_USER_USER_NAME=$OPENCHS_UPLOAD_USER_USER_NAME OPENCHS_UPLOAD_USER_PASSWORD=$OPENCHS_UPLOAD_USER_PASSWORD pm2 start app.js --name rules-server --update-env"
