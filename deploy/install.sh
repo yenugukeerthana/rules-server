403644ada2877c034ecea977ad8c8ec06616d659
Joy A <itsjoy@gmail.com>
2023-04-14 14:36:15 +0530

    avniproject/avni-webapp#874 | Set TZ env var for rules server process

--- deploy/install.sh
+++ deploy/install.sh
@@ -18,4 +18,4 @@ fi
 chown -R rules:rules /opt/rules-server
 chgrp -R rules /opt/rules-server
 chmod -R 770 /opt/rules-server
-sudo -H -u rules bash -c "pm2 stop rules-server && cd /opt/rules-server && OPENCHS_UPLOAD_USER_USER_NAME=$1 OPENCHS_UPLOAD_USER_PASSWORD=$2 AVNI_TZ=$3 NODE_ENV=production pm2 start app.js --name rules-server --update-env"
+sudo -H -u rules bash -c "pm2 stop rules-server && cd /opt/rules-server && OPENCHS_UPLOAD_USER_USER_NAME=$1 OPENCHS_UPLOAD_USER_PASSWORD=$2 TZ=$3 NODE_ENV=production pm2 start app.js --name rules-server --update-env"
